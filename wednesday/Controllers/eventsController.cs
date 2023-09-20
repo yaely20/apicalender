e0ad387a15d3717fa2b52ae540b6dfb90097c308
yaely20 <y0583248523@gmail.com>
2023-09-20 23:59:59 +0300

    my commit message

---
+++ wednesday/Controllers/eventsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace wednesday.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class eventsController : ControllerBase
+    {
+        // GET: api/<eventsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<eventsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<eventsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<eventsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<eventsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ wednesday/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace wednesday
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+
+}
